529e4db76facbd1d3c3659dc9d0bad1c9b98d7e0
Navidoo <navidta@gmail.com>
2025-07-01 01:29:28 +0330

    fix exceptions issues

--- job_scheduler/exceptions.py
+++ job_scheduler/exceptions.py
@@ -2,30 +2,33 @@ from fastapi import HTTPException, status
 
 
 class AppException(HTTPException):
-    def __init__(self, detail: str, error_code: str, status_code: int):
-        super().__init__(status_code=status_code, detail={"detail": detail, "error_code": error_code})
+    status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail: str = "Application Exception"
+    error_code: str = "APP_EXCEPTION"
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail={"detail": self.detail, "error_code": self.error_code})
 
 
-TaskNotFound = AppException(
-    detail="Task not found",
-    error_code="TASK_404",
-    status_code=status.HTTP_404_NOT_FOUND,
-)
+class TaskNotFound(AppException):
+    status_code = status.HTTP_404_NOT_FOUND
+    detail = "Task not found"
+    error_code = "TASK_404"
 
-SchedulerRemovalFailed = AppException(
-    detail="Failed to remove task from scheduler",
-    error_code="SCHEDULER_500",
-    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-)
 
-TaskCreationFailed = lambda e: AppException(
-    detail=f"Failed to create/schedule task: {e}",
-    error_code="TASK_CREATE_500",
-    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-)
+class TaskCreationFailed(AppException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Failed to create/schedule task"
+    error_code = "TASK_CREATE_500"
 
-TaskDeletionFailed = lambda e: AppException(
-    detail=f"Failed to delete task: {e}",
-    error_code="TASK_DELETE_500",
-    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-)
+
+class SchedulerRemovalFailed(AppException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Failed to remove task from scheduler"
+    error_code = "SCHEDULER_500"
+
+
+class TaskDeletionFailed(AppException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = f"Failed to delete task"
+    error_code = "TASK_DELETE_500"
